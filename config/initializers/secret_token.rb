b002e063f752ab407d873250805f79ed59da451f
Patrick Byrne <patrick.byrne@tstmedia.com>
2013-03-12 17:46:46 -0500

    bare rails 3.2.12 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TimelineSpike::Application.config.secret_token = '88330dfc876dc77572a5b93307ca5aec6eb21b7247d92afa1b19314209df1064f65a04f9d19bc573091d3493e9d0e0adf2bf934424dcbee1e5b97aeeaa308bc4'
