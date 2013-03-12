92ada6246b198c32c32200615aa21807c99fc9b7
Patrick Byrne <patrick.byrne@tstmedia.com>
2013-03-12 18:39:04 -0500

    show the entries by default

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 TimelineSpike::Application.routes.draw do
   resources :entries
 
+  root "entries#index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
