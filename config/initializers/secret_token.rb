ed6772c16f70f5314ba05a9166ac1bc1e7f8b9fc
Patrick Byrne <patrick.byrne@tstmedia.com>
2013-03-12 18:28:37 -0500

    add bare rails 4.0.0b1 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TimelineSpike::Application.config.secret_key_base = '8f975ce60d5d7b36661bf0be83365a7428a72c5a5791d758d940e11eac2ec2878f200870183af70d838d708bf12f19efdd7cb8959af8baf6a53f5365d577d13a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TimelineSpike::Application.config.session_store :encrypted_cookie_store, key: '_timeline_spike_session'
